5918a89586a1f914a26f377595837c7560305b50
Logan Ellis <mrloganellis@gmail.com>
2019-09-15 21:45:50 -0400

    Updated JS File, on the right track!

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-//$(':submit').on('click', function() {
-    //when text is submitted then add it to the list//
-    
-
-
+/*
 $('.shopping-item-toggle').on('click', function () {
-   $('.shopping-item').addClass('.shopping-item__checked');
+   $('.shopping-item').addClass('shopping-item__checked');
 }
 )
 
@@ -12,4 +8,36 @@ $('.shopping-item-delete').on('click', function() {
     $('shopping-item').remove();
     return ('The delete button ran!');
 }
-)
+)*/
+function itemAdd() {
+    $('#js-shopping-list-form').on('submit', function(e) {
+       // when text is submitted then add it to the list//
+        e.preventDefault();
+
+        let item = $('#shopping-list-entry').val();
+
+        let element = `<li>
+                        <span class="shopping-item">${item}</span>
+                        <div class="shopping-item-controls">
+                        <button class="shopping-item-toggle">
+                            <span class="button-label">check</span>
+                        </button>
+                        <button class="shopping-item-delete">
+                            <span class="button-label">delete</span>
+                        </button>
+                        </div>
+                    </li>`;
+
+      $('.shopping-list').append(element);
+
+      $('#shopping-list-entry').val("");
+
+    });
+}
+
+
+function initialize() {
+    itemAdd();
+}
+
+$(initialize);
